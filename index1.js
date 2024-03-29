0be9daadc5f837096cb90e0a0030ef602e6ee1cb
MONJUR21 <monjur.csecu@gmail.com>
2024-03-30 04:57:11 +0600

    e

--- index1.js
+++ index1.js
@@ -1,13 +1,23 @@
-obj={
-    name: "Monju",
-    age: 25,
-    city: "Bangalore"
-}
-let array=["name", "age", "city","country","east", "south"];
-for (const key in obj) {
-    console.log(obj[key]);
-}
-console.log("\n");
-for (const iterator of array) {
-    console.log(iterator);
-}
+// obj={
+//     name: "Monju",
+//     age: 25,
+//     city: "Bangalore"
+// }
+// let array=["name", "age", "city","country","east", "south"];
+// for (const key in obj) {
+//     console.log(obj[key]);
+// }
+// console.log("\n");
+// for (const iterator of array) {
+//     console.log(iterator);
+// const arr=[1,2,5,7,89,32];
+// console.log(arr);
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     console.log(element);    
+// }
+// console.log(typeof(arr));
+let array=[2,3,4,5,6];
+array.forEach((element)=>{
+    console.log(element);
+});
